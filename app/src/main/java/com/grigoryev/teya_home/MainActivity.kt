5c2f963b14fe4a07db1ed897022fb836e84e48e1
Valera <valera@Mac-Book-Pro-16-VG.local>
2025-07-23 23:05:20 +0100

    appearance alignment

--- app/src/main/java/com/grigoryev/teya_home/MainActivity.kt
+++ app/src/main/java/com/grigoryev/teya_home/MainActivity.kt
@@ -3,8 +3,6 @@ package com.grigoryev.teya_home
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.commit
 import com.grigoryev.teya_home.album.list.presentation.AlbumListScreenFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,12 +13,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
         
         if (savedInstanceState == null) {
             supportFragmentManager.commit {
